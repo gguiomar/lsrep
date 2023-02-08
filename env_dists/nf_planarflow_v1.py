acec721ebe697ea7fd2e5539e9cafdb16491eec1
Gonçalo Guiomar <goncalo.guiomar@gmail.com>
2023-02-08 15:31:57 +0000

    started exploring the flow layers

--- env_dists/nf_planarflow_v1.py
+++ env_dists/nf_planarflow_v1.py
@@ -23,7 +23,7 @@ for i in range(K):
     flows += [nf.flows.Planar((2,))]
 target = nf.distributions.TwoModes(2, 0.1)
 
-q0 = nf.distributions.DiagGaussian(2)
+q0 = nf.distributions.DiagGaussian(2) # what's the dimensionality of these samples?
 nfm = nf.NormalizingFlow(q0=q0, flows=flows, p=target)
 nfm.to(device)
 
@@ -94,3 +94,60 @@ z_np = z.to('cpu').data.numpy()
 plt.figure(figsize=(10, 10))
 plt.hist2d(z_np[:, 0].flatten(), z_np[:, 1].flatten(), (grid_size, grid_size), range=[[-3, 3], [-3, 3]])
 plt.show()
+# %%
+
+# %%
+
+num_samples = 1000
+layer_n = 0
+z_test  = [[nfm.flows[layer_n].forward(e)[0].detach().numpy(), 
+            nfm.flows[layer_n].forward(e)[1].detach().numpy()] 
+            for e in np.random.normal(0,10,num_samples)]
+
+xy = np.zeros((len(z_test),2))
+zz = np.zeros(len(z_test))
+
+for i,e in enumerate(z_test):
+    xy[i,:] = e[0][0]
+    zz[i] = e[1]
+
+#%%
+# plot the 2d hist of xy
+plt.figure(figsize=(10, 10))
+plt.hist2d(xy[:,0], xy[:,1], (grid_size, grid_size), range=[[-3, 3], [-3, 3]])
+plt.show()
+
+#%%
+# plot the 2d scatter of xy
+plt.figure(figsize=(10, 10))
+plt.scatter(xy[:,0], xy[:,1], c=zz)
+plt.show()
+
+# %%
+
+# function that plots the 2d hist of xy for a given layer
+##### why is this giving me lines?
+
+def plot_hist_layer(layer_n, num_samples, grid_size):
+    z_test  = [[nfm.flows[layer_n].forward(e)[0].detach().numpy(), 
+                nfm.flows[layer_n].forward(e)[1].detach().numpy()] 
+                for e in np.random.normal(0,10,num_samples)]
+
+    xy = np.zeros((len(z_test),2))
+    zz = np.zeros(len(z_test))
+
+    for i,e in enumerate(z_test):
+        xy[i,:] = e[0][0]
+        zz[i] = e[1]
+
+    plt.figure(figsize=(10, 10))
+    plt.hist2d(xy[:,0], xy[:,1], (grid_size, grid_size), range=[[-3, 3], [-3, 3]])
+    plt.show()
+
+num_samples = 1000
+for layer in range(K):
+    plot_hist_layer(layer, num_samples, grid_size=200)
+
+
+
+
